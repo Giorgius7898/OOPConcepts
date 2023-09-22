1a7c001b553a414d3b07a3ca677ffccaa9779b11
Jorge78998 <jorge.ocampo21956@ucaldas.edu.co>
2023-09-22 08:03:41 -0500

    Actualización

--- src/domain/model/Shape.java
+++ src/domain/model/Shape.java
@@ -1,17 +1,28 @@
 package domain.model;
 
 public class Shape {
-    public int x = 0;
-    public int y = 0;
+    private int x = 0;
+    private int y = 0;
     public Shape(int newX, int newY){
-        x = newX;
-        y = newY;
+        setX(newX);
+        setY(newY);
     }
     public Shape(){
 
     }
-    public int getAdd(){
-        return x+y;
+    public void setX(int newX){
+        if (newX>=0 && newX<=1000)
+            x=newX;
+    }
+    public int getX(){
+        return x;
+    }
+    public void setY(int newY){
+        if (newY>=0 && newY<=1000)
+            y=newY;
+    }
+    public int Y(){
+        return y;
     }
 }
 
--- src/domain/ui/Test.java
+++ src/domain/ui/Test.java
@@ -4,12 +4,10 @@ import domain.model.Shape;
 
 public class Test {
     public static void main(String[] args) {
-        var shape1 = new Shape(10,20);
-        var shape2 = new Shape();
-        System.out.println("Ins #1 x = "+shape1.x+ ", y = "+shape1.y);
-        System.out.println("Ins #2 x = "+shape2.x+ ", y = "+shape2.y);
+        var shape1 = new Shape(-200,10);
+        var shape2 = new Shape(20,1500);
 
-        System.out.println("Sum Ins#1 = "+shape1.getAdd());
-        System.out.println("Sum Ins#2 = "+shape2.getAdd());
+        System.out.println(shape1.getX());
+
     }
 }
